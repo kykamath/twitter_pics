7841452d6726708ae77e4a31a2c43395ac42b434
kykamath <krishna.kamath@gmail.com>
2011-03-11 19:49:50 -0600

    Modified parser.

---
+++ src/parser.py
@@ -0,0 +1,34 @@
+'''
+Created on Mar 11, 2011
+
+@author: kykamath
+'''
+import cjson, sys, gzip
+from settings import Settings
+
+class Utilities:
+    @staticmethod
+    def tweetInBoundingBox(tweet, bb=[30, 42, 129, 145]):                                                                                                                                            
+        if not tweet.get('coordinates'): return False                                                                                                                        
+        lng,lat = tweet['coordinates']['coordinates']                                                                                                                    
+        if bb[0]<lat<bb[1] and bb[2]<lng<bb[3]: return True
+    @staticmethod
+    def iterateTweetsFromGzip(file):
+        for line in gzip.open(file, 'rb'): 
+            try:
+                data = cjson.decode(line)
+                if 'text' in data: yield data
+            except: pass
+
+
+class Parser:
+    @staticmethod
+    def getTweetsForJapan():
+        japan_bb=[30, 42, 129, 145]
+        for f in ['2011_3_10.gz']:
+            for tweet in Utilities.iterateTweetsFromGzip(Settings.filter_folder+f):
+                if Utilities.tweetInBoundingBox(tweet, japan_bb):
+                    print cjson.encode(tweet)
+
+if __name__ == '__main__':
+    Parser.getTweetsForJapan()
---
+++ src/settings.py
@@ -0,0 +1,7 @@
+'''
+Created on Mar 11, 2011
+
+@author: kykamath
+'''
+class Settings:
+    filter_folder = '/mnt/chevron/kykamath/data/twitter/filter/'
