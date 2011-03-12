b324969b6556e32652b0fdf9594d9954b1e072e9
kykamath <krishna.kamath@gmail.com>
2011-03-12 16:02:39 -0600

    Modified parser.

--- src/parser.py
+++ src/parser.py
@@ -84,6 +84,7 @@ class Parser:
             for tweet in Utilities.iterateTweetsFromFile(Settings.japan_pics_folder+'tweets/'+f):
                 d = datetime.strptime(tweet['created_at'], Settings.twitter_api_time_format)
                 print d
+                print cjson.encode(tweet)
                 service, url = 'twitpic', tweet['entities']['urls'][0]['url']
                 url = url.replace('\\', '')
                 for service, parseMethod in services.iteritems():
