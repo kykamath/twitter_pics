9484009f69e785e1fd1263686b881ed51284e246
kykamath <krishna.kamath@gmail.com>
2011-03-12 15:59:05 -0600

    Modified parser.

--- src/parser.py
+++ src/parser.py
@@ -89,7 +89,7 @@ class Parser:
                 for service, parseMethod in services.iteritems():
                     if service in url:
                         id = tweet['id']
-                        fileName = Settings.japan_pics_folder+Utilities.getDataFile(d)+'/%s_%s'%(str(d).replace(' ', '_'), id)
+                        fileName = Settings.japan_pics_folder+Utilities.getDataFile(d)+'/%s_%s.jpeg'%(str(d).replace(' ', '_'), id)
                         print fileName
                         folder = '/'.join(fileName.split('/')[:-1])
                         if not os.path.exists(folder): os.makedirs(folder, 0777)
